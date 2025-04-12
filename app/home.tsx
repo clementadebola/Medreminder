178b805bb7fa997d41bae7a5fc236385ef457834
clement <clementadebola54@gmail.com>
2025-04-12 21:18:04 +0100

    new

--- app/home.tsx
+++ app/home.tsx
@@ -1,11 +1,25 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import {useState, useEffect, useRef, useCallback} from 'react';
 import { View, Text, StyleSheet, Animated } from "react-native";
 
 
 export default function HomeScreen() {
   return (
+     <LinearGradient colors={["#4CAF50", "#2E7D52FF"]} style={styles.container}>
    <View >
-    <Text>Home</Text>
+    <Text style={{color:'#fff'}}>Home page</Text>
    </View>
+   </LinearGradient>
   )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+      // backgroundColor: '#4caf50'
+    },
+
+})
